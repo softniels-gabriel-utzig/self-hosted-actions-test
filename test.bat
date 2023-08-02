0ffcf8da62df6b0ca2fc460b8f8dab9374c971e1
Gabriel <gabriel.utzig@softniels.com.br>
2023-08-02 17:23:04 -0300

    Update test.bat

--- test.bat
+++ test.bat
@@ -1,13 +1,1 @@
-@echo off
-set LogFile=log.txt
-
-REM Verifica se o arquivo de log existe
-if not exist %LogFile% (
-    echo Criando o arquivo de log...
-    echo Log gerado em: %date% %time% > %LogFile%
-) else (
-    echo Adicionando entrada de log...
-    echo Log gerado em: %date% %time% >> %LogFile%
-)
-
-echo Log registrado com sucesso!
+Spotify.exe
